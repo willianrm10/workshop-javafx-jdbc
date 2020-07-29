17adc065964277b1a5a2dad3e14313ccf7ab2cc6
Willian Moraes <willianrm@yahoo.com.br>
2020-07-29 14:38:10 -0300

    TextField & DatePicker

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,8 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -17,6 +20,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Seller;
@@ -24,44 +28,55 @@ import model.exceptions.ValidationException;
 import model.services.SellerService;
 
 public class SellerFormController implements Initializable {
-	
+
 	private Seller entity;
 	private SellerService service;
-	
+
 	private List<DataChangeListener> datachangeListeners = new ArrayList<>();
-	
+
 	@FXML
 	private TextField txtId;
 	@FXML
 	private TextField txtName;
-	
+	@FXML
+	private TextField txtEmail;
+	@FXML
+	private DatePicker dpBirthDate;
+	@FXML
+	private TextField txtBasesalary;
+
 	@FXML
 	private Label labelErrorName;
+	@FXML
+	private Label labelErrorEmail;
+	@FXML
+	private Label labelErrorBarthDate;
+	@FXML
+	private Label labelErrorBaseSalary;
+
 	@FXML
 	private Button btSave;
 	@FXML
 	private Button btCancel;
-	
-	
+
 	public void setSeller(Seller entity) {
 		this.entity = entity;
 	}
-	
+
 	public void setSellerService(SellerService service) {
 		this.service = service;
 	}
-	
+
 	public void subscribeDataChangeListener(DataChangeListener listener) {
 		datachangeListeners.add(listener);
 	}
-	
-	
+
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
-		if(entity == null) {
+		if (entity == null) {
 			throw new IllegalStateException("Entity was nulll");
 		}
-		if(service == null) {
+		if (service == null) {
 			throw new IllegalStateException("Service was nulll");
 		}
 		try {
@@ -69,40 +84,37 @@ public class SellerFormController implements Initializable {
 			service.saveOrUpdate(entity);
 			notifydataChangeListeners();
 			Utils.currentStage(event).close();
-		}
-		catch(ValidationException e) {
+		} catch (ValidationException e) {
 			setErrorMessages(e.getErrors());
-		}
-		catch(DbException e) {
+		} catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
-		
+
 	}
-	
+
 	private void notifydataChangeListeners() {
-		for(DataChangeListener listener : datachangeListeners) {
+		for (DataChangeListener listener : datachangeListeners) {
 			listener.onDataChanged();
 		}
-		
+
 	}
 
 	private Seller getFormData() {
 		Seller obj = new Seller();
-		
+
 		ValidationException excetion = new ValidationException("Validation error");
-		
-		
+
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
-		
-		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
+
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
 			excetion.addError("Name", "O campo não pode ser vazio");
 		}
 		obj.setName(txtName.getText());
-		
-		if(excetion.getErrors().size() > 0) {
+
+		if (excetion.getErrors().size() > 0) {
 			throw excetion;
 		}
-		
+
 		return obj;
 	}
 
@@ -114,31 +126,39 @@ public class SellerFormController implements Initializable {
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
-		
+
 	}
-	
-	private void initializeNodes() {//limita os caracteres a ser informado no campo de texto
+
+	private void initializeNodes() {// limita os caracteres a ser informado no campo de texto
 		Constraints.setTextFieldInteger(txtId);
-		Constraints.setTextFieldMaxLength(txtName, 30);
+		Constraints.setTextFieldMaxLength(txtName, 70);
+		Constraints.setTextFieldDouble(txtBasesalary);
+		Constraints.setTextFieldMaxLength(txtEmail, 60);
+		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
 	}
 
 	public void updateFormData() {
-		if(entity == null) {
+		if (entity == null) {
 			throw new IllegalStateException("Entity was null");
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+		txtEmail.setText(entity.getEmail());
+		Locale.setDefault(Locale.US);
+		txtBasesalary.setText(String.format("%.2f", entity.getBaseSalary()));
+		if (entity.getBirthDate() != null) {
+			dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
+		}
 	}
-	
+
 	private void setErrorMessages(Map<String, String> errors) {
 		Set<String> fields = errors.keySet();
-		
-		if(fields.contains("Name")){
+
+		if (fields.contains("Name")) {
 			labelErrorName.setText(errors.get("Name"));
-			
+
 		}
-				
+
 	}
-	
-	
+
 }
